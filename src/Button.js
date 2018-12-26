fdcab48139811e859d4536b0e95258c8dc45a075
Vestimir Markov <me@vestimir.com>
2018-12-26 15:04:22 +0200

    Update button

--- src/Button.js
+++ src/Button.js
@@ -1,17 +1,12 @@
 import React from 'react';
-// import { Link } from '../../routes';
-
-const Link = () => <React.Fragment>{children}</React.Fragment>;
 
 const BUTTON_TYPES = {
   primary: 'btn-primary',
   secondary: 'btn-secondary'
 };
 
-const Button = ({ label, type = 'primary', routeName, params, onClick }) => (
-  <Link route={routeName} params={params}>
-    <a className={BUTTON_TYPES[type]} onClick={onClick}>{label}</a>
-  </Link>
+const Button = ({ label, children, type = 'primary', onClick }) => (
+  <a className={BUTTON_TYPES[type]} onClick={onClick}>{children || label}</a>
 );
 
 export default Button;
