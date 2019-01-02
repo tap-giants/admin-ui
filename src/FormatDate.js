6cb46f4638d1d56295eb698bd976f4a263467539
Nikolay Slavov <nikolay@slavov.org>
2019-01-02 19:04:48 +0200

    Fix date format

--- src/FormatDate.js
+++ src/FormatDate.js
@@ -5,7 +5,7 @@ const FormatDate = ({ format, children }) =>
   <span className="format-date">{dayjs(children).format(format)}</span>;
 
 FormatDate.defaultProps = {
-  format: 'll',
+  format: 'MMM D, YYYY',
 };
 
 export default FormatDate;
